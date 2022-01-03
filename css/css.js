f8c076dc48b761af626c1a9d2d2864dec7ac91c3
qaiswardag <qais.wardag@outlook.com>
2022-01-03 13:59:59 +0100

    Update style.css

---
+++ css/css.js
@@ -0,0 +1,9 @@
+'use strict';
+// Make mobile navigation work
+const headerNavigation = document.querySelector('.header');
+const headerMobileMenuItem = document.querySelector('.header-mobile-menu-item');
+
+// headerMobileMenuItem.addEventListener('click', function () {
+//   console.log('test');
+//   headerNavigation.classList.toggle('nav-open');
+// });
