4b01e8be42ec56eadbfc2f8e9c3fefcb7c9fa971
Qais Wardag <47112831+qaiswardag@users.noreply.github.com>
2022-05-22 13:30:32 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
-console.log("Hello world!");
-
-const myName = "Jonas Schmedtmann";
 const h1 = document.querySelector(".heading-primary");
-console.log(myName);
 console.log(h1);
 
 // h1.addEventListener("click", function () {
-//   h1.textContent = myName;
-//   h1.style.backgroundColor = "red";
 //   h1.style.padding = "5rem";
 // });
 
